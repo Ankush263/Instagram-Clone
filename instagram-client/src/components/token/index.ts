a621e829cde08767fb8502e37fbdefc6d7744050
Ankush263 <ankushbanik263@gmail.com>
2023-08-31 15:57:27 +0530

    fix token

--- instagram-client/src/components/token/index.ts
+++ instagram-client/src/components/token/index.ts
@@ -1,13 +1,17 @@
 export const fetchToken = (): string => {
-	try {
-		const tokenObj = localStorage.getItem('Token') || null;
-		let Token;
-		if (tokenObj) {
-			Token = JSON.parse(tokenObj).value;
+	if (typeof window !== 'undefined') {
+		try {
+			const tokenObj = localStorage.getItem('Token') || null;
+			let Token;
+			if (tokenObj) {
+				Token = JSON.parse(tokenObj).value;
+			}
+			return Token || '';
+		} catch (error) {
+			console.log(error);
+			return '';
 		}
-		return Token;
-	} catch (error) {
-		console.log(error);
+	} else {
 		return '';
 	}
 };
